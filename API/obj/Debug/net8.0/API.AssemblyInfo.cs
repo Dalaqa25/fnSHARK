9c2e2f0efa90c76b0c87e36d401515f180000e14
dalaqa25 <g.dalaqishvili01@gmail.com>
2024-08-03 11:42:13 +0400

    Temporary commit to switch branches

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+989d411b6c55ad47413a6cc358ac3a3ca79972e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ff70ec31b5db9fbf6fda3547468af092ba2906c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
